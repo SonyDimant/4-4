659e5f780562c4dc5971b7788637ebf9f5ab72ec
Sophizm <dimantsofa@gmail.com>
2024-05-17 22:41:44 +0300

    22

--- 4-4/Models/Blogs.cs
+++ 4-4/Models/Blogs.cs
@@ -10,14 +10,14 @@ namespace _4_4.ViewModels
         public string Text { get; set; }
         public string Title { get; set; }
         public string[] Tags { get; set; }
-        //public string Imagepath { get; set; }
+        public string Imagepath { get; set; }
 
-        public Blog(string title, string text, string[] tags)
+        public Blog(string title, string text, string[] tags, string image)
         {
             Title = title;
             Text = text;
             Tags = tags;
-            //Imagepath = image;
+            Imagepath = image;
         }
     }
 }
